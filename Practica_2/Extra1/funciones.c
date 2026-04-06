f32e0e1d9e54709b927a4eaa0169b3fc6948be72
miguelijoo <miguelijo07@gmail.com>
2026-04-06 22:55:44 +0200

    Continuar ejercicio extra 1

--- Practica_2/Extra1/funciones.c
+++ Practica_2/Extra1/funciones.c
@@ -6,11 +6,27 @@
 float **crearimagen(int alto, int ancho){
     float **imagen=NULL;
     imagen=(float **) calloc(alto, sizeof(float*));
-    for(int i=0;i<alto;i++){
-        imagen[i]=(float*) calloc(ancho, sizeof(float));
+    if(imagen==NULL){
+        return NULL;
     }
+    imagen[0]=(float *) calloc(alto*ancho, sizeof(float)); //Creamos todos los espacios de memoria de la matriz en la fila 0.
+    if(imagen[0]==NULL){
+        return NULL;
+    }
+    for(int i=1;i<alto;i++){
+        imagen[i]=imagen[i-1]+ancho;
+        /* * Como cada imagen[i] es un puntero (una fila), hacemos que apunten 
+        * justo donde acaba el ancho de la fila anterior. 
+        * * Ejemplo con alto=3 y ancho=3:
+        * - imagen[0] contiene el bloque gigante de 9 casillas consecutivas.
+        * - imagen[1] no empieza al principio, sino 3 casillas más adelante.
+         * - imagen[2] apuntará 3 casillas después de donde empieza imagen[1].
+        * * Así, cada fila lee solo los datos que le corresponden dentro de la línea recta.
+        */
+    }
+    return imagen;
 }
 
-void generarruido(float **imagen){
-    
+void generarruido(float **imagen, int alto, int ancho){
+
 }
--- Practica_2/Extra1/main.c
+++ Practica_2/Extra1/main.c
@@ -4,10 +4,15 @@
 #include "funciones.h"
 
 int main(){
+    srand(time(NULL));
     printf("Especifica el alto y ancho de la imagen a crear: \n");
     int ancho=0, alto=0;
     scanf("%d", &alto);
     scanf("%d", &ancho);
     float **imagen=crearimagen(alto, ancho);
-
+    if(imagen==NULL){
+        printf("No hay memoria suficiente");
+        return -1;
+    }
+    generarruido(imagen, alto, ancho);
 }
