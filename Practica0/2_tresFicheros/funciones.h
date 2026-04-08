f61ad3bf71c1c5488f7fdd1dcacdf985e5222112
miguelijoo <miguelijo07@gmail.com>
2026-04-08 23:03:50 +0200

    Actualizacion general

--- Practica0/2_tresFicheros/funciones.h
+++
@@ -1,4 +0,0 @@
-//En los ficheros .h incluiremos #defines, struct y prototipos (cabeceras) de funciones
-
-//cabecera de la funcion esPar
-int esPar(int numero);
--- Practica0/4_vectoresEnteros/funciones2.h
+++
@@ -1,4 +0,0 @@
-#define MAX_VECTOR 15
-void rellenarVector(int Vector[], int nEle);
-void imprimirVector(int Vector[], int nEle);
-float calcularMedia(int Vector[], int nEle);
